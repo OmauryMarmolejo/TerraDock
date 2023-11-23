59c15679b9ff13eb8639f0ca8c687157f49d58d2
Omaury Marmolejo <omaury@tango.io>
2023-11-23 15:38:05 -0600

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -15,15 +15,13 @@ data "template_file" "cb_app" {
 }
 
 resource "aws_ecs_task_definition" "app" {
-  family             = "cb-app-task"
-  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
-  network_mode       = "awsvpc"
-  requires_compatibilities = [
-    "FARGATE"
-  ]
-  cpu                   = var.fargate_cpu
-  memory                = var.fargate_memory
-  container_definitions = data.template_file.cb_app.rendered
+  family                   = "cb-app-task"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = var.fargate_cpu
+  memory                   = var.fargate_memory
+  container_definitions    = data.template_file.cb_app.rendered
 }
 
 resource "aws_ecs_service" "main" {
@@ -46,5 +44,4 @@ resource "aws_ecs_service" "main" {
   }
 
   depends_on = [aws_alb_listener.front_end, aws_iam_role_policy_attachment.ecs-task-execution-role-policy-attachment]
-
 }
